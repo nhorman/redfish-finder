d607eaf706efea693d34fb37f314452225cc1adf
Neil Horman <nhorman@tuxdriver.com>
2018-08-14 15:52:50 -0400

    Adding USBNetDevice implementation
    Signed-off-by: Neil Horman <nhorman@tuxdriver.com>

--- redfish-finder.py
+++ redfish-finder.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 
+import os
 import subprocess
 
 def cursor_consume_next(cursor, needle):
@@ -12,9 +13,56 @@ class NetDevice(object):
 	def __init(self):
 		self.name = "Unknown"
 
+	def getifcname(self):
+		return self.name
+
 class USBNetDevice(NetDevice):
 	def __init__(self, dmioutput):
 		super(NetDevice, self).__init__()
+		dmioutput = cursor_consume_next(dmioutput, "idVendor: ")
+		# Strip the 0x off the front of the vendor and product ids
+		self.vendor = (dmioutput.split()[0])[2:]
+		self.product = (dmioutput.split()[2])[2:]
+
+		# Now we need to find the corresponding device name in sysfs
+		if self.find_device() == False:
+			return None
+
+	def getifcname(self, dpath):
+		for root, dirs, files in os.walk(dpath, topdown=False):
+			for d in dirs:
+				try:
+					if d != "net":
+						continue
+					dr = os.listdir(os.path.join(root, d))
+					self.name = dr[0]
+					return True
+				except:
+					continue
+		return False
+
+	def find_device(self):
+		for root,dirs,files in os.walk("/sys/bus/usb/devices", topdown=False):
+			for d in dirs:
+				try:
+					f = open(os.path.join(root, d, "idVendor"))
+					lines = f.readlines()
+					if lines[0][0:4] != self.vendor:
+						f.close()
+						continue
+					f.close()
+					f = open(os.path.join(root, d, "idProduct"))
+					lines = f.readlines()
+					if lines[0][0:4] != self.product:
+						f.close()
+						continue
+					f.close()
+					# We found a matching product and vendor, go find the net directory
+					# and get the interface name
+					return self.getifcname(os.path.join(root, d))
+				except:
+					continue
+		return False 
 
 class dmiobject():
 	def __init__(self, dmioutput):
